f3c85de0e46db4b0fcf6e15125635660840f855f
AgusLozano99 <aguspro99@gmail.com>
2022-08-15 20:06:50 -0300

    heroku

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const path = require("path");
 const app = express();
 app.use(express.static('./public'));
 
-app.listen(3000, () => {
-  console.log("Servidor corriendo en el puerto 3000");
-});
+app.listen(process.env.PORT || 3000, function(){
+  console.log('Servidor corriendo en el puerto 3000')
+})
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/home.html"));
